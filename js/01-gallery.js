958ff23b05c344909937e16289c3046e13b8602a
Andrij Csuhran <4usika@gmail.com>
2023-05-21 20:29:04 +0200

    1 task completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+let modalInstance = null; // Змінна для збереження посилання на відкрите модальне вікно
+
+// Рендер розмітки елементів галереї
+const renderGallery = () => {
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            data-source="${original}" 
+            alt="${description}" />
+            </a>
+            </li>`;
+    });
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(''));
+};
+
+renderGallery();
+
+// Обробник кліку на елементах галереї
+const handleGalleryClick = (event) => {
+    event.preventDefault();
+    let clickedItemEl = event.target;
+        if (clickedItemEl.nodeName !== 'IMG') {
+    return;
+}
+
+    let bigImage = clickedItemEl.dataset.source;
+    openBigImage(bigImage);
+};
+
+gallery.addEventListener('click', handleGalleryClick);
+
+function openBigImage(largeImageUrl) {
+    const modal = basicLightbox.create(
+        `<img src="${largeImageUrl}" width="800" height="600" alt="" />`,
+        { onShow: () => {
+            document.addEventListener('keydown', handleKeyDown); // Прослуховувач події натискання клавіші
+        },
+        onClose: () => {
+            document.removeEventListener('keydown', handleKeyDown); // Видалити прослуховувач події натискання клавіші
+            modalInstance = null;
+        },
+        });
+
+    modal.show();
+    modalInstance = modal;
+    }
+
+function handleKeyDown(event) {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+}
+
+function closeModal() {
+    if (modalInstance) {
+    modalInstance.close();
+    }
+}
